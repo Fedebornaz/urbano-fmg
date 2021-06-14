706ffe07c01afc048bf5af0caa8dd959e024949f
fedebornaz <fedebnz@gmail.com>
2021-06-13 22:55:10 -0300

    style: add animation on hero.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 2.5s ease-out",
+      },
       colors: {
         pink: {
           darkest: "#7e343e",
